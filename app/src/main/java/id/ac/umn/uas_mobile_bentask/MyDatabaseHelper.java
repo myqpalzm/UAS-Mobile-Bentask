e1e9d2a339e61b94737b3a222963b8071bd1c7e3
Gabrielsar <76233399+Gabrielsar@users.noreply.github.com>
2021-11-24 18:32:45 +0700

    Menambah tabel task dan foreign key ke id category

--- app/src/main/java/id/ac/umn/uas_mobile_bentask/MyDatabaseHelper.java
+++ app/src/main/java/id/ac/umn/uas_mobile_bentask/MyDatabaseHelper.java
@@ -14,8 +14,19 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "ToDo.db";
     private static final int DATABASE_VERSION = 1;
     private static final String TABLE_NAME = "my_category";
+    private static final String TABLE_TASK = "task";
+
     private static final String COLUMN_ID = "_id";
     private static final String COLUMN_TITLE = "category_name";
+
+    private static final String taskId = "taskId";
+    private static final String taskTitle = "taskTitle";
+    private static final String taskDescription = "taskDesription";
+    private static final String isComplete = "isComplete";
+    private static final String firstAlarmTime = "firstAlarmTime";
+    private static final String secondAlarmTime = "secondAlarmTime";
+    private static final String lastAlarm = "lastAlarm";
+    private static final String categoryId = "categoryId";
 
     MyDatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -24,14 +35,27 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query = "CREATE TABLE " + TABLE_NAME +
+        String query1 = "CREATE TABLE " + TABLE_NAME +
                 " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 COLUMN_TITLE + " TEXT);";
-        db.execSQL(query);
+        db.execSQL(query1);
+        String query2 = "CREATE TABLE "
+                + TABLE_TASK + " ("
+                + taskId + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + taskTitle + " TEXT, "
+                + taskDescription + " TEXT, "
+                + isComplete + " BOOLEAN, "
+                + firstAlarmTime + " TEXT, "
+                + secondAlarmTime + " TEXT, "
+                + lastAlarm + " TEXT, "
+                + categoryId + " INTEGER, "
+                + " FOREIGN KEY (" + categoryId + ") REFERENCES " + TABLE_NAME + "(" + COLUMN_ID + "));";
+        db.execSQL(query2);
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
         onCreate(db);
     }
 
